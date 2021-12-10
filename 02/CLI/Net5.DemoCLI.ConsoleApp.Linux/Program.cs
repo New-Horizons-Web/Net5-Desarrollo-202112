e00840be1fa0a0fa9901b46276a4b8f227c3becb
Erick Arostegui Cunza <scorpius86@gmail.com>
2021-12-10 02:48:24 +0100

    Session 02

---
+++ 02/CLI/Net5.DemoCLI.ConsoleApp.Linux/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net5.DemoCLI.ConsoleApp.Linux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Erick!");
+        }
+    }
+}
---
+++ 02/LogAndTrace/Net5.LogAndTrace/Net5.LogAndTrace.Web.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net5.LogAndTrace.Web.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Esto es un dato informativo");
+            _logger.LogWarning("Esto es un dato de advertencia");
+            _logger.LogError("Esto es un dato de error");
+
+
+            var rng = new Random();
+            MyMethod();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        private void MyMethod()
+        {
+            Console.WriteLine("MyMethod");
+        }
+    }
+}
