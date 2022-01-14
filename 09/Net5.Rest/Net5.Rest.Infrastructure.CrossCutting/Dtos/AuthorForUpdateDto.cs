7db62f230beb4d6a95c359927acfad1dc7ac60f7
Erick Arostegui Cunza <scorpius86@gmail.com>
2022-01-14 02:08:50 +0100

    Session 09

---
+++ 09/Net5.Rest/Net5.Rest.Infrastructure.CrossCutting/Dtos/AuthorForUpdateDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net5.Rest.Infrastructure.CrossCutting.Dtos
+{
+    public class AuthorForUpdateDto
+    {
+        public AuthorForUpdateDto()
+        {
+            Books = new List<BookForUpdateDto>();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string Genre { get; set; }
+
+        public virtual ICollection<BookForUpdateDto> Books { get; set; }
+    }
+}
