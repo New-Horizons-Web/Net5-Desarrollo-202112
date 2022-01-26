3e3c18acb11e21b5a4fa406769bf8539ad9982d1
Erick Arostegui Cunza <scorpius86@gmail.com>
2022-01-26 02:51:33 +0100

    Add API

---
+++ 12/Net5.SignalR.Chat/Net5.SignalR.Chat.BackEnd/Infrastructure/Data/Migrations/20220126014400_InitData.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Net5.SignalR.Chat.BackEnd.Infrastructure.Data.Migrations
+{
+    public partial class InitData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomName = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.RoomId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chat",
+                columns: table => new
+                {
+                    ChatId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Message = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chat", x => x.ChatId);
+                    table.ForeignKey(
+                        name: "FK_ChatRoom",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "RoomId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ChatUser",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomUser",
+                columns: table => new
+                {
+                    RoomUserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomUser", x => x.RoomUserId);
+                    table.ForeignKey(
+                        name: "FK_RoomUserRoom",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "RoomId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RoomUserUser",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chat_RoomId",
+                table: "Chat",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chat_UserId",
+                table: "Chat",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Room__6B500B558C2ADB86",
+                table: "Room",
+                column: "RoomName",
+                unique: true,
+                filter: "[RoomName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomUser_RoomId",
+                table: "RoomUser",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomUser_UserId",
+                table: "RoomUser",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__User__C9F284568175C925",
+                table: "User",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chat");
+
+            migrationBuilder.DropTable(
+                name: "RoomUser");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
