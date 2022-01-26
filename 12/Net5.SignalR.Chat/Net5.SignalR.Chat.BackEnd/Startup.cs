25a4c0c89e9d33375ab3609711f6111186088f25
Erick Arostegui Cunza <scorpius86@gmail.com>
2022-01-26 02:42:05 +0100

    Add EF

--- 12/Net5.SignalR.Chat/Net5.SignalR.Chat.BackEnd/Startup.cs
+++ 12/Net5.SignalR.Chat/Net5.SignalR.Chat.BackEnd/Startup.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Net5.SignalR.Chat.BackEnd.Infrastructure.Data.Contexts;
+using Net5.SignalR.Chat.BackEnd.Infrastructure.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,12 +29,17 @@ namespace Net5.SignalR.Chat.BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<SignalRChatContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("SignalRChatConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Net5.SignalR.Chat.BackEnd", Version = "v1" });
             });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+            services.AddScoped<IChatRepository, ChatRepository>();
+            services.AddScoped<IRoomUserRepository, RoomUserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
